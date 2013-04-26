390db019c48c9afd1ceb30eb11d88360493fd688
JohnCheng <johncheng@linpus.com>
2013-04-26 15:01:43 +0800

    add debug level helper

---
+++ src/tw/h4/game/escape/Debugger.java
@@ -0,0 +1,38 @@
+package tw.h4.game.escape;
+
+import android.util.Log;
+
+public class Debugger {
+    private static final String TAG = "Escape-";
+    private static final int VERBOSE = 0x0001;
+    private static final int DEBUG = 0x0002;
+    private static final int INFO = 0x0004;
+    private static final int WARN = 0x0008;
+    private static final int ERROR = 0x0010;
+    private static final int LOG_LEVEL = 0x001F;
+
+    public static void v(String tag, String msg) {
+        if (0 != (LOG_LEVEL & VERBOSE))
+            Log.v(TAG + tag, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (0 != (LOG_LEVEL & DEBUG))
+            Log.v(TAG + tag, msg);
+    }
+
+    public static void i(String tag, String msg) {
+        if (0 != (LOG_LEVEL & INFO))
+            Log.i(TAG + tag, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        if (0 != (LOG_LEVEL & WARN))
+            Log.w(TAG + tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (0 != (LOG_LEVEL & ERROR))
+            Log.e(TAG + tag, msg);
+    }
+}
--- src/tw/h4/game/escape/SignalReceiver.java
+++ src/tw/h4/game/escape/SignalReceiver.java
@@ -7,20 +7,21 @@ import android.content.Intent;
 import android.util.Log;
 
 public class SignalReceiver extends BroadcastReceiver {
-	public static final String DISASTER_START = "tw.h4.game.escape.DISASTER_START";
+    public static final String DISASTER_START = "tw.h4.game.escape.DISASTER_START";
+    private static final String TAG = "SignalReceiver";
 
-	@Override
-	public void onReceive(Context ctx, Intent intent) {
-		String action = intent.getAction();
-		Log.d("TAG", "get action::" + action);
-		if (DISASTER_START.equals(action)) {
-			Intent activity = new Intent(ctx, WarningActivity.class);
-			activity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			ctx.startActivity(activity);
-			// TODO: launch WarningActivity
-		} else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
-			// TODO: check the disaster start time
-		}
-	}
+    @Override
+    public void onReceive(Context ctx, Intent intent) {
+        String action = intent.getAction();
+        Debugger.d(TAG, "get action::" + action);
+        if (DISASTER_START.equals(action)) {
+            Intent activity = new Intent(ctx, WarningActivity.class);
+            activity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ctx.startActivity(activity);
+            // TODO: launch WarningActivity
+        } else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
+            // TODO: check the disaster start time
+        }
+    }
 
 }
